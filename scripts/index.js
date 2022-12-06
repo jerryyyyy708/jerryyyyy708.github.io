add4997a628cc2cd386a2dd7d09256afb4339ca9
jerryyyyy708 <jerryyyyy708@gmail.com>
2022-12-06 22:40:58 +0800

    add site body

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+
+let jerry = "jerry"
+console.log(jerry)
+// 小數點有沒有都是console
+
+let ben = "ben"
+
+let jb = jerry + ben
+console.log(jb)
+
+let ry = {
+    aa: "ddd0",
+    bb: 200,
+    cc: "ada",
+    dd: [1, 2, 3],
+}
+console.log(ry)
+
+let rz = {
+    ry,
+    ry,
+    ry,
+}
+
+let asa = 3 === 31
+
+let jb2 = [jerry, jerry, ben]
+console.log(jb2[1])
+jerry = "fff0"
+console.log(jb2[1])
+
+//let runner = document.querySelector('h1')
+//runner.textContent = 'uuu'
+
+
