9c282e19c55c4d1b59e2e556ec19cca8edb5f202
jerryyyyy708 <jerryyyyy708@gmail.com>
2023-10-08 21:31:13 +0800

    Update VST Project

--- temp.py
+++ temp.py
@@ -5,7 +5,7 @@ def update_page_value_by_title(db_path, title_value, page_value):
     """Update the 'Page' value based on a given 'Title'."""
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("UPDATE projects SET Page = ? WHERE Title = ?", (page_value, title_value))
+    cursor.execute("UPDATE projects SET Title = ? WHERE Title = ?", (page_value, title_value))
     conn.commit()
     conn.close()
     print(f"Updated 'Page' value to '{page_value}' for project titled '{title_value}'!")
@@ -14,5 +14,6 @@ def update_page_value_by_title(db_path, title_value, page_value):
 db_path = 'projects.db'
 #titles = ['Android Song Writer', 'Sketch Converter', 'VST Plugin', 'NYCU CS Project', 'YT Data Analysis']
 #htmls = ['android.html', 'sketch.html', 'vst.html', 'fyp.html', 'YT_vis.html']
-for i in range(len(titles)):
-    update_page_value_by_title(db_path, titles[i], htmls[i])
+update_page_value_by_title(db_path, 'Audio Converter VST', 'VST Plugin')
+# for i in range(len(titles)):
+#     update_page_value_by_title(db_path, titles[i], htmls[i])
